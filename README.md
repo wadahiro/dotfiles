0e694485b690834517574b649442f97324128c2e
Hiroyuki Wada <wadahiro@gmail.com>
2017-01-16 17:55:35 +0900

    Add README and setup-apps script

---
+++ README.md
@@ -0,0 +1,7 @@
+# @wadahiro's dotfiles
+
+## Setup
+
+1. Clone `https://github.com/wadahiro/dotfiles.git`
+2. Run `setup-apps.sh`
+3. Run `setup.sh`
---
+++ setup-apps.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# Package management
+pacman -S yaourt
+
+# Window manager
+pacman -S xorg-server xorg-server-utils xorg-xinit xorg-xclock xterm
+pacman -S i3-wm i3status dmenu rofi compton lxappearance
+pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
+#pacman -S networkmanager network-manager-applet gnome-keyring wpa_supplicant
+
+systemctl enable lightdm
+
+# VirtualBox guest
+pacman -S virtualbox-guest-utils
+
+# Filer
+pacman -S spacefm
+pacman -S numix-themes
+yaourt -S numix-icon-theme-git
+
+# Terminal
+pacman -S termite
+
+systemctl enable sshd
+systemctl start sshd
+
+# SSH
+pacman -S openssh
+
+# Git
+pacman -S git
+pacman -S tig
+
+# Languages
+pacman -S python python-pip python2
+pacman -S jdk6 jdk7-openjdk jdk8-openjdk
+pacman -S nodejs npm
+pacman -S go
+pacman -S rustup
+
+# Utilities
+pacman -S unzip
+pacman -S copyq
+pacman -S xsel
+pacman -S feh
+pacman -S imwheel
+pacman -S powerline python-powerline
+
+# Japanese
+pacman -S fcitx-im fcitx-configtool fcitx-mozc
+
+# Fonts
+echo << EOS >> /etc/pacman.conf
+[infinality-bundle]
+Server = http://bohoomil.com/repo/$arch
+EOS
+pacman-key -r 962DDE58
+pacman-key --lsign-key 962DDE58
+pacman -Syy
+pacman -S infinality-bundle
+pacman -S ttf-dejavu ttf-hack otf-ipafont powerline-fonts 
+yaourt -S otf-source-han-code-jp
+
+# Zsh
+pacman -S prezto zsh
+
+# Vim
+pacman -S vim
+
+# Tmux
+pacman -S tmux
+
+# Browser
+pacman -S google-chrome-beta
+
+# Dev tools
+pacman -S docker
+systemctl enable docker
+systemctl
+pacman -S visual-studio-code-insiders
+pacman -S eclim eclipse-java
+pacman -S maven
+yaourt -S yarn
+
