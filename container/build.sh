071ac4abaef6496ad8e86379ba10e09a737277de
Hiroyuki Wada <wadahiro@gmail.com>
2019-08-25 16:58:14 +0900

    Add lxde

--- container/build.sh
+++ container/build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-VERSION=ubuntu19.04_ja
+VERSION=ubuntu19.04
 
 docker build -f Dockerfile.base . -t wadahiro/docker-guidev-base:$VERSION
+docker build -f Dockerfile.base.ja . -t wadahiro/docker-guidev-base:${VERSION}_ja
+
 docker build -f Dockerfile.i3wm --build-arg BASE_VERSION=$VERSION . -t wadahiro/docker-guidev-i3wm:$VERSION
+docker build -f Dockerfile.i3wm --build-arg BASE_VERSION=${VERSION}_ja . -t wadahiro/docker-guidev-i3wm:${VERSION}_ja
+
+docker build -f Dockerfile.lxde --build-arg BASE_VERSION=$VERSION . -t wadahiro/docker-guidev-lxde:$VERSION
+docker build -f Dockerfile.lxde --build-arg BASE_VERSION=${VERSION}_ja . -t wadahiro/docker-guidev-lxde:${VERSION}_ja
