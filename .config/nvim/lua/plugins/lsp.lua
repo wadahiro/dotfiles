e57ba7e52a7aad3a4d7e205ec4bd04376a1b5bea
Hiroyuki Wada <wadahiro@gmail.com>
2026-01-10 01:20:08 +0900

    Migrate to Neovim 0.11 native LSP API
    - Remove nvim-lspconfig, use vim.lsp.config/vim.lsp.enable
    - Explicitly specify cmd, filetypes, root_markers for each server
    - Configure absolute paths to mason-installed binaries
    - Support gopls, ts_ls, eslint, rust_analyzer, jdtls, lua_ls

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -11,50 +11,94 @@ return {
       "williamboman/mason.nvim",
     },
     config = function()
-      require("mason-lspconfig").setup({
+      local mason_lspconfig = require("mason-lspconfig")
+
+      -- Ensure servers are installed
+      mason_lspconfig.setup({
         ensure_installed = {
+          "gopls",
           "ts_ls",
           "eslint",
           "rust_analyzer",
-          "lua_ls",
           "jdtls",
-          "gopls",
+          "lua_ls",
         },
-        automatic_installation = true,
       })
 
-      -- Configure Lua LSP for Neovim development
-      vim.lsp.config("lua_ls", {
+      -- Configure LSP servers using native vim.lsp.config
+      vim.lsp.config("*", {
+        root_markers = { ".git" },
+      })
+
+      vim.lsp.config.gopls = {
+        cmd = { vim.fn.stdpath("data") .. "/mason/bin/gopls" },
+        filetypes = { "go", "gomod", "gowork", "gotmpl" },
+        root_markers = { "go.mod", ".git" },
+        settings = {
+          gopls = {
+            analyses = {
+              unusedparams = true,
+            },
+            staticcheck = true,
+            gofumpt = true,
+          },
+        },
+      }
+
+      vim.lsp.config.ts_ls = {
+        cmd = { vim.fn.stdpath("data") .. "/mason/bin/typescript-language-server", "--stdio" },
+        filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+        root_markers = { "package.json", "tsconfig.json", "jsconfig.json", ".git" },
+      }
+
+      vim.lsp.config.eslint = {
+        cmd = { vim.fn.stdpath("data") .. "/mason/bin/vscode-eslint-language-server", "--stdio" },
+        filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx", "vue", "svelte", "astro" },
+        root_markers = { ".eslintrc", ".eslintrc.js", ".eslintrc.json", "package.json", ".git" },
+      }
+
+      vim.lsp.config.rust_analyzer = {
+        cmd = { vim.fn.stdpath("data") .. "/mason/bin/rust-analyzer" },
+        filetypes = { "rust" },
+        root_markers = { "Cargo.toml", "rust-project.json", ".git" },
+      }
+
+      vim.lsp.config.jdtls = {
+        cmd = { vim.fn.stdpath("data") .. "/mason/bin/jdtls" },
+        filetypes = { "java" },
+        root_markers = { "pom.xml", "build.gradle", ".git" },
+      }
+
+      vim.lsp.config.lua_ls = {
+        cmd = { vim.fn.stdpath("data") .. "/mason/bin/lua-language-server" },
+        filetypes = { "lua" },
+        root_markers = { ".luarc.json", ".luarc.jsonc", ".luacheckrc", ".stylua.toml", "stylua.toml", "selene.toml", "selene.yml", ".git" },
         settings = {
           Lua = {
-            runtime = {
-              version = "LuaJIT",
-            },
-            diagnostics = {
-              globals = { "vim" },
-            },
+            runtime = { version = "LuaJIT" },
+            diagnostics = { globals = { "vim" } },
             workspace = {
               library = vim.api.nvim_get_runtime_file("", true),
               checkThirdParty = false,
             },
-            telemetry = {
-              enable = false,
-            },
+            telemetry = { enable = false },
           },
         },
-      })
+      }
 
-      -- Enable language servers
-      vim.lsp.enable({ "ts_ls", "eslint", "rust_analyzer", "lua_ls", "jdtls", "gopls" })
+      -- Enable LSP servers
+      vim.lsp.enable({ "gopls", "ts_ls", "eslint", "rust_analyzer", "jdtls", "lua_ls" })
 
-      -- LspAttach autocmd for keymaps and settings
+      -- LspAttach autocmd for keymaps
       vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(args)
           local bufnr = args.buf
           local client = vim.lsp.get_client_by_id(args.data.client_id)
 
           -- Enable native completion
-          vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = true })
+          if client and client.supports_method("textDocument/completion") then
+            vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = true })
+          end
 
           -- Keymaps
           local opts = { buffer = bufnr, silent = true, noremap = true }
