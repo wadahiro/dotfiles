e895878a1fe5064c13c7e75b4aa48f0e0350aa7e
Hiroyuki Wada <wadahiro@gmail.com>
2026-01-10 00:31:11 +0900

    Add fish shell configuration and migrate to mise for runtime management

---
+++ .config/fish/conf.d/ghq_key_bindings.fish
@@ -0,0 +1,4 @@
+bind \c] '__ghq_repository_search'
+if bind -M insert >/dev/null 2>/dev/null
+    bind -M insert \c] '__ghq_repository_search'
+end
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,58 +1,24 @@
+# FZF settings
 set -U FZF_LEGACY_KEYBINDINGS 0
 set -U FZF_REVERSE_ISEARCH_OPTS "--reverse --height=100%"
 
-set -x set LC_ALL en_JP
+# Locale
+set -x LC_ALL en_JP
 
-#set PATH $HOME/bin $HOME/.anyenv/bin $PATH
-#eval (anyenv init - fish | source)
-
+# PATH
 set -x PATH $GOPATH/bin $PATH
 
-# Lima
-set -x DOCKER_HOST unix://$HOME/.lima/dev1/sock/docker.sock 
-# Colima
-#set -x DOCKER_HOST unix://$HOME/.colima/default/docker.sock
-set -x DOCKER_BUILDKIT 1
-
+# Aliases
 alias vi 'nvim'
 alias vim 'nvim'
 alias vimdiff 'nvim -d'
 alias sed 'gsed'
 alias dbash 'docker exec -it (docker ps --format "table {{.Names}}\t{{.Image}}" | fzf | awk \'{print $1}\') /bin/bash'
 
+# Docker
+set -x DOCKER_BUILDKIT 1
 
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
-
-# asdf
-#source /usr/local/opt/asdf/libexec/asdf.fish
-#. ~/.asdf/plugins/golang/set-env.fish
-#. ~/.asdf/plugins/java/set-java-home.fish
-#set --export RUST_WITHOUT rust-docs
-
-
-# mise
-#~/.local/bin/mise activate fish | source
-
-
-
-# pnpm
-set -gx PNPM_HOME "/Users/h2-wada/Library/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
-
-# Zellij
-# The following snippet is meant to be used like this in your fish config:
-#
-# if status is-interactive
-#     # Configure auto-attach/exit to your likings (default is off).
-#     # set ZELLIJ_AUTO_ATTACH true
-#     # set ZELLIJ_AUTO_EXIT true
-#     eval (zellij setup --generate-auto-start fish | string collect)
-# end
+# Zellij auto-start
 if not set -q ZELLIJ
     if test "$ZELLIJ_AUTO_ATTACH" = "true"
         zellij attach -c
@@ -65,3 +31,7 @@ if not set -q ZELLIJ
     end
 end
 
+# Load local configuration (not tracked in git)
+if test -f ~/.config/fish/config.local.fish
+    source ~/.config/fish/config.local.fish
+end
