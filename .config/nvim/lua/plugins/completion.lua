bbf68aad1b2d333d1e6104e67fba88f287ab879c
Hiroyuki Wada <wadahiro@gmail.com>
2026-01-10 20:52:54 +0900

    Add blink.cmp for code completion

---
+++ .config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "saghen/blink.cmp",
+    version = "1.*",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+    },
+    opts = {
+      keymap = { preset = "default" },
+      appearance = {
+        nerd_font_variant = "mono",
+      },
+      completion = {
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 200,
+        },
+      },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+      signature = {
+        enabled = true,
+      },
+    },
+  },
+}
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -93,12 +93,6 @@ return {
       vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(args)
           local bufnr = args.buf
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-
-          -- Enable native completion
-          if client and client.supports_method("textDocument/completion") then
-            vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = true })
-          end
 
           -- Keymaps
           local opts = { buffer = bufnr, silent = true, noremap = true }
